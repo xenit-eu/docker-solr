5b2e6fdb59bb8b853c5f740434ff7a15a28dfe5e
Robrecht Vanhuysse <robrecht.vanhuysse@xenit.eu>
2020-09-01 16:19:29 +0200

    DOCKER-342 make repo cert alias configurable for 91-healthcheck.sh in solr6

--- solr6/local/91-healthcheck.sh
+++ solr6/local/91-healthcheck.sh
@@ -6,6 +6,7 @@ SOLR_DIR_ROOT="${SOLR_INSTALL_HOME}/solrhome"
 ALFRESCO_SSL=${ALFRESCO_SSL:-'https'}
 SSL_KEY_STORE=${SSL_KEY_STORE:-'ssl.repo.client.keystore'}
 SSL_KEY_STORE_PASSWORD=${SSL_KEY_STORE_PASSWORD:-'kT9X6oe68t'}
+SSL_KEY_STORE_ALIAS=${SSL_KEY_STORE_ALIAS:-'ssl.repo'}
 
 JETTY_PORT=${JETTY_PORT:-'8080'}
 JETTY_PORT_SSL=${JETTY_PORT_SSL:-'8443'}
@@ -21,7 +22,7 @@ if [ $ALFRESCO_SSL != none ]
 then
     if [ ! -f "${SOLR_DIR_ROOT}/keystore/browser.p12" ]
     then
-	keytool -importkeystore -srckeystore "${SOLR_DIR_ROOT}/keystore/${SSL_KEY_STORE}" -srcstorepass ${SSL_KEY_STORE_PASSWORD} -srcstoretype JCEKS -srcalias ssl.repo -destkeystore  "${SOLR_DIR_ROOT}/keystore/browser.p12" -deststoretype pkcs12 -destalias ssl.repo -deststorepass alfresco -destkeypass alfresco
+	keytool -importkeystore -srckeystore "${SOLR_DIR_ROOT}/keystore/${SSL_KEY_STORE}" -srcstorepass ${SSL_KEY_STORE_PASSWORD} -srcstoretype JCEKS -srcalias ${SSL_KEY_STORE_ALIAS} -destkeystore  "${SOLR_DIR_ROOT}/keystore/browser.p12" -deststoretype pkcs12 -destalias ssl.repo -deststorepass alfresco -destkeypass alfresco
 	openssl pkcs12 -in "${SOLR_DIR_ROOT}/keystore/browser.p12" -out "${SOLR_DIR_ROOT}/keystore/browser.pem" -nodes -passin pass:alfresco
     fi
     echo "curl -f -k -L -E ${SOLR_DIR_ROOT}/keystore/browser.pem https://localhost:${PORT}/solr >/dev/null" >${SOLR_INSTALL_HOME}/healthcheck.sh
