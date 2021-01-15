2738fab401320c5680ef50b18750835f0a1b350d
anghelutar <anghelutar@yahoo.com>
2021-01-15 22:34:32 +0100

    More elaborate healthchecks.

--- upstream/solr6/local/91-healthcheck.sh
+++ upstream/solr6/local/91-healthcheck.sh
@@ -17,6 +17,7 @@ else
 fi
 export PORT
 
+echo "#!/bin/bash" >${SOLR_INSTALL_HOME}/healthcheck.sh
 if [ $ALFRESCO_SSL != none ]
 then
     if [ ! -f "${SOLR_DIR_ROOT}/keystore/browser.p12" ]
@@ -24,10 +25,12 @@ then
 	keytool -importkeystore -srckeystore "${SOLR_DIR_ROOT}/keystore/${SSL_KEY_STORE}" -srcstorepass ${SSL_KEY_STORE_PASSWORD} -srcstoretype JCEKS -srcalias ssl.repo -destkeystore  "${SOLR_DIR_ROOT}/keystore/browser.p12" -deststoretype pkcs12 -destalias ssl.repo -deststorepass alfresco -destkeypass alfresco
 	openssl pkcs12 -in "${SOLR_DIR_ROOT}/keystore/browser.p12" -out "${SOLR_DIR_ROOT}/keystore/browser.pem" -nodes -passin pass:alfresco
     fi
-    echo "curl -f -k -L -E ${SOLR_DIR_ROOT}/keystore/browser.pem https://localhost:${PORT}/solr >/dev/null" >${SOLR_INSTALL_HOME}/healthcheck.sh
+    # for custom certificates, replace the browser.pem with certificates able to talk to solr    
+    echo "status=\$(curl -f -k -L -w %{http_code} -s -E ${SOLR_DIR_ROOT}/keystore/browser.pem -o /dev/null https://localhost:${PORT}/solr)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 else
-    echo "curl -f -L  http://localhost:${PORT}/solr >/dev/null" >${SOLR_INSTALL_HOME}/healthcheck.sh    
+    echo "status=\$(curl -f -L -w %{http_code} -s -o /dev/null http://localhost:${PORT}/solr)" >>${SOLR_INSTALL_HOME}/healthcheck.sh 
 fi
+echo "if [[ \"\$status\" -ne 200 ]] ; then exit 1 ; else exit 0 ; fi" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 
 echo "Solr init healthcheck end"        
 
