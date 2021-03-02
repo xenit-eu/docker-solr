7bd9012bfc7c32edccf4faac12ae513f8e5dc513
anghelutar <anghelutar@yahoo.com>
2021-03-02 01:53:27 +0100

    Healthcheck testing first alfresco-core health, working also for the sharded setup. Removed license from sharded setup, works also without.

--- upstream/solr4/local/91-healthcheck.sh
+++ upstream/solr4/local/91-healthcheck.sh
@@ -6,6 +6,9 @@ ALFRESCO_SSL=${ALFRESCO_SSL:-'https'}
 SSL_KEY_STORE=${SSL_KEY_STORE:-'ssl.repo.client.keystore'}
 SSL_KEY_STORE_PASSWORD=${SSL_KEY_STORE_PASSWORD:-'kT9X6oe68t'}
 
+CORES_ALFRESCO=${CORES_ALFRESCO:-"alfresco"}
+IFS=';' read -r -a DEFAULT_CORES_ALFRESCO <<< "$CORES_ALFRESCO"
+
 TOMCAT_PORT=${TOMCAT_PORT:-'8080'}
 TOMCAT_PORT_SSL=${TOMCAT_PORT_SSL:-'8443'}
 if [ $ALFRESCO_SSL != none ]
@@ -26,9 +29,9 @@ then
         keytool -importkeystore -srckeystore "${SOLR_INSTALL_HOME}/keystore/${SSL_KEY_STORE}" -srcstorepass ${SSL_KEY_STORE_PASSWORD} -srcstoretype JCEKS -srcalias ssl.repo -destkeystore  "${SOLR_INSTALL_HOME}/keystore/browser.p12" -deststoretype pkcs12 -destalias ssl.repo -deststorepass alfresco -destkeypass alfresco
         openssl pkcs12 -in "${SOLR_INSTALL_HOME}/keystore/browser.p12" -out "${SOLR_INSTALL_HOME}/keystore/browser.pem" -nodes -passin pass:alfresco
     fi
-    echo "status=\$(curl -f -k -L -w %{http_code} -s -E ${SOLR_INSTALL_HOME}/keystore/browser.pem -o /dev/null https://localhost:${PORT}/solr4)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
+    echo "status=\$(curl -f -k -L -w %{http_code} -s -E ${SOLR_INSTALL_HOME}/keystore/browser.pem -o /dev/null https://localhost:${PORT}/solr4/${DEFAULT_CORES_ALFRESCO[0]}/admin/ping)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 else
-        echo "status=\$(curl -f -L -w %{http_code} -s -o /dev/null http://localhost:${PORT}/solr4)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
+        echo "status=\$(curl -f -L -w %{http_code} -s -o /dev/null http://localhost:${PORT}/solr4/${DEFAULT_CORES_ALFRESCO[0]}/admin/ping)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 fi
 echo "if [[ \"\$status\" -ne 200 ]] ; then exit 1 ; else exit 0 ; fi" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 
--- upstream/solr6/local/91-healthcheck.sh
+++ upstream/solr6/local/91-healthcheck.sh
@@ -7,6 +7,9 @@ ALFRESCO_SSL=${ALFRESCO_SSL:-'https'}
 SSL_KEY_STORE=${SSL_KEY_STORE:-'ssl.repo.client.keystore'}
 SSL_KEY_STORE_PASSWORD=${SSL_KEY_STORE_PASSWORD:-'kT9X6oe68t'}
 
+CORES_ALFRESCO=${CORES_ALFRESCO:-"alfresco"}
+IFS=';' read -r -a DEFAULT_CORES_ALFRESCO <<< "$CORES_ALFRESCO"
+
 JETTY_PORT=${JETTY_PORT:-'8080'}
 JETTY_PORT_SSL=${JETTY_PORT_SSL:-'8443'}
 if [ $ALFRESCO_SSL != none ]
@@ -27,9 +30,9 @@ then
 	    keytool -importkeystore -srckeystore "${SOLR_DIR_ROOT}/keystore/${SSL_KEY_STORE}" -srcstorepass ${SSL_KEY_STORE_PASSWORD} -srcstoretype JCEKS -srcalias ssl.repo -destkeystore  "${SOLR_DIR_ROOT}/keystore/browser.p12" -deststoretype pkcs12 -destalias ssl.repo -deststorepass alfresco -destkeypass alfresco
 	    openssl pkcs12 -in "${SOLR_DIR_ROOT}/keystore/browser.p12" -out "${SOLR_DIR_ROOT}/keystore/browser.pem" -nodes -passin pass:alfresco
     fi
-    echo "status=\$(curl -f -k -L -w %{http_code} -s -E ${SOLR_DIR_ROOT}/keystore/browser.pem -o /dev/null https://localhost:${PORT}/solr)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
+    echo "status=\$(curl -f -k -L -w %{http_code} -s -E ${SOLR_DIR_ROOT}/keystore/browser.pem -o /dev/null https://localhost:${PORT}/solr/${DEFAULT_CORES_ALFRESCO[0]}/admin/ping)" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 else
-    echo "status=\$(curl -f -L -w %{http_code} -s -o /dev/null http://localhost:${PORT}/solr)" >>${SOLR_INSTALL_HOME}/healthcheck.sh 
+    echo "status=\$(curl -f -L -w %{http_code} -s -o /dev/null http://localhost:${PORT}/solr/${DEFAULT_CORES_ALFRESCO[0]}/admin/ping)" >>${SOLR_INSTALL_HOME}/healthcheck.sh 
 fi
 echo "if [[ \"\$status\" -ne 200 ]] ; then exit 1 ; else exit 0 ; fi" >>${SOLR_INSTALL_HOME}/healthcheck.sh
 
