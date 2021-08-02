1337b32239026a7210f7cca4eef843d7d2b32a4e
Roxana Angheluta <anghelutar@yahoo.com>
2021-08-02 19:29:07 +0200

    ALFREDOPS-769 Add defaults for backup variables. Get snapshot name from Swarm. Improvements restore script.

--- xenit/solr6/local/93-restore-from-backup.sh
+++ xenit/solr6/local/93-restore-from-backup.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-BACKUP_NAME=${BACKUP_NAME:-`date '+%Y-%m-%d'`}
-
 echo "Solr restore from backup start"
 
-if [ -z $RESTORE_FROM_BACKUP ]
+if [ -z ${RESTORE_FROM_BACKUP} ]
 then
     echo "The index should not be restored from backup, exiting"
 else
     # if data folder is empty restore from backup
-    # backup name mandatory
-    if [ -d "${SOLR_DATA_ROOT}/index/alfresco/index" ]
+    if [ -d "${SOLR_DATA_ROOT}/index/alfresco/index" -a `du -s "${SOLR_DATA_ROOT}/index/alfresco/index" | cut -f 1` -gt 100 ]
     then
         echo "Index folder existing, skipping backup restore"
         ls -ltr "${SOLR_DATA_ROOT}/index/alfresco/index"
@@ -22,64 +19,56 @@ else
         gosu "${user}" "${SOLR_INSTALL_HOME}/solr/bin/solr" start -m "${JAVA_XMX}" -p "${PORT}" -h "${SOLR_HOST}" -s "${SOLR_DIR_ROOT}" -a "${JAVA_OPTS}"
         sleep 30
         echo "*************** solr without tracking started **************************"
-        # restore from backup
-        if [ $ALFRESCO_SSL != none ]
+        # get the name of latest snapshot to be restored from the backup
+        if [ -z ${RESTORE_BACKUP_NAME} ]
         then
-            # try to restore from backups from last 3 days
-            for i in 1 2 3
-            do
-	            echo "Calling restore command for ${BACKUP_NAME}"
-	            curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=s3&location=s3:///&name=${BACKUP_NAME}"
+            RESTORE_BACKUP_NAME=`curl -s -L -XGET -u ${BACKUP_USERNAME}:${BACKUP_PASSWORD} "http://${BACKUP_ENDPOINT}/${BACKUP_BUCKET}/success?domain=${BACKUP_DOMAIN}" | tail -1 | cut -d '.' -f 2`
+        fi
+        echo "RESTORE_BACKUP_NAME=${RESTORE_BACKUP_NAME}"
+        if [ ${RESTORE_BACKUP_NAME} != "snapshot"* ]
+        then
+           echo "Could not find a valid snapshot to restore from, exiting"
+        else
+            if [ $ALFRESCO_SSL != none ]
+            then
+	            echo "Calling restore command for ${RESTORE_BACKUP_NAME}"
+	            curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///&name=${RESTORE_BACKUP_NAME}"
 
 	            restorestatus=$(curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
 
-	            if [ ! "\"In Progress\"" = ${restorestatus} ]
-	            then
-                    BACKUP_NAME=`date -d "$BACKUP_NAME - 1 day" '+%Y-%m-%d'`
-	                continue
-	            fi
 	            # wait until restore is complete
 	            while [ "\"In Progress\"" = ${restorestatus} ]
 	            do
 	                echo "restorestatus=$restorestatus"
 	                restorestatus=$(curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
 	                sleep 5
-	                done
-	            if [ ! "\"success\"" = ${restorestatus} ]
-	            then
-                    echo "Restore successful"
-                    break
-	            fi
-	            BACKUP_NAME=`date -d "$BACKUP_NAME - 1 day" '+%Y-%m-%d'`
-            done
-        else
-            # try to restore from backups from last 3 days
-            for i in 1 2 3
-                do
-	            echo "Calling restore command for ${BACKUP_NAME}"
-	            curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=s3&location=s3:///&name=${BACKUP_NAME}"
-	            restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
-	            if [ ! "\"In Progress\"" = ${restorestatus} ]
-	            then
-	                BACKUP_NAME=`date -d "$BACKUP_NAME - 1 day" '+%Y-%m-%d'`
-	                continue
-	            fi
-	            # wait until restore is complete
-	            while [ "\"In Progress\"" = ${restorestatus} ]
-	            do
-	                echo "restorestatus=$restorestatus"
-	                restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
-	                sleep 5
 	            done
-	        	if [ ! "\"success\"" = ${restorestatus} ]
+	            if [  "\"success\"" = ${restorestatus} ]
 	            then
                     echo "Restore successful"
-                    break
+                else
+                    echo "Restore was not successful"
 	            fi
-	            BACKUP_NAME=`date -d "$BACKUP_NAME - 1 day" '+%Y-%m-%d'`
-            done
+            else
+                echo "Calling restore command for ${RESTORE_BACKUP_NAME}"
+                curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///&name=${RESTORE_BACKUP_NAME}"
+                restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
+                # wait until restore is complete
+                while [ "\"In Progress\"" = ${restorestatus} ]
+                do
+                    echo "restorestatus=$restorestatus"
+                    restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
+                    sleep 5
+                done
+        	    if [  "\"success\"" = ${restorestatus} ]
+                then
+                       echo "Restore successful"
+                else
+                       echo "Restore was not successful"
+                fi
+            fi
         fi
-
+        
         # stop solr
         gosu ${user} ${SOLR_INSTALL_HOME}/solr/bin/solr stop
         sleep 30
