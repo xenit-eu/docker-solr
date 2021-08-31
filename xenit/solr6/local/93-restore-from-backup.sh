653c2c9690bfe100aa92250ccd354e8e0f69b4aa
Roxana Angheluta <anghelutar@yahoo.com>
2021-08-31 17:21:16 +0200

    ALFREDOPS-769 Snapshot to be restored is detected automatically in case no parameter is given to the restore request. No "success" file anymore.

--- xenit/solr6/local/93-restore-from-backup.sh
+++ xenit/solr6/local/93-restore-from-backup.sh
@@ -21,53 +21,50 @@ else
         # get the name of latest snapshot to be restored from the backup
         if [ -z ${RESTORE_BACKUP_NAME} ]
         then
-            RESTORE_BACKUP_NAME=`curl -s -L -XGET -u ${BACKUP_USERNAME}:${BACKUP_PASSWORD} "http://${BACKUP_ENDPOINT}/${BACKUP_BUCKET}/success?domain=${BACKUP_DOMAIN}" | tail -1 | cut -d '.' -f 2`
-        fi
-        echo "RESTORE_BACKUP_NAME=${RESTORE_BACKUP_NAME}"
-        if [[ ! ${RESTORE_BACKUP_NAME} == "20"* ]]
-        then
-           echo "Could not find a valid snapshot to restore from, exiting"
+            restorename=""
         else
-            if [ $ALFRESCO_SSL != none ]
-            then
-	            echo "Calling restore command for ${RESTORE_BACKUP_NAME}"
-	            curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///&name=${RESTORE_BACKUP_NAME}"
+            restorename="&name=${RESTORE_BACKUP_NAME}"
+        fi
 
+        if [ $ALFRESCO_SSL != none ]
+        then
+	        echo "Calling restore command curl -s -k -E ${SOLR_DIR_ROOT}/keystore/browser.pem https://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///${restorename}"
+	        curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///${restorename}"
+
+	        restorestatus=$(curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
+
+	        # wait until restore is complete
+	        while [ "\"In Progress\"" = ${restorestatus} ]
+	        do
+	            echo "restorestatus=$restorestatus"
 	            restorestatus=$(curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
-
-	            # wait until restore is complete
-	            while [ "\"In Progress\"" = ${restorestatus} ]
-	            do
-	                echo "restorestatus=$restorestatus"
-	                restorestatus=$(curl -s -k -E "${SOLR_DIR_ROOT}/keystore/browser.pem" "https://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
-	                sleep 5
-	            done
-	            if [  "\"success\"" = ${restorestatus} ]
-	            then
-                    echo "Restore successful"
-                else
-                    echo "Restore was not successful"
-	            fi
+	            sleep 5
+	        done
+	        if [  "\"success\"" = ${restorestatus} ]
+	        then
+                echo "Restore successful"
             else
-                echo "Calling restore command for ${RESTORE_BACKUP_NAME}"
-                curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///&name=${RESTORE_BACKUP_NAME}"
+                echo "Restore was not successful"
+	        fi
+        else
+            echo "Calling restore command curl -s http://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///${restorename}"
+            curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restore&repository=swarm&location=swarm:///${restorename}"
+            restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
+            # wait until restore is complete
+            while [ "\"In Progress\"" = ${restorestatus} ]
+            do
+                echo "restorestatus=$restorestatus"
                 restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
-                # wait until restore is complete
-                while [ "\"In Progress\"" = ${restorestatus} ]
-                do
-                    echo "restorestatus=$restorestatus"
-                    restorestatus=$(curl -s "http://localhost:${PORT}/solr/alfresco/replication?command=restorestatus&wt=json" | jq .restorestatus.status)
-                    sleep 5
-                done
-        	    if [  "\"success\"" = ${restorestatus} ]
-                then
-                       echo "Restore successful"
-                else
-                       echo "Restore was not successful"
-                fi
+                sleep 5
+            done
+        	if [  "\"success\"" = ${restorestatus} ]
+            then
+                   echo "Restore successful"
+            else
+                   echo "Restore was not successful"
             fi
         fi
-        
+
         # stop solr
         gosu ${user} ${SOLR_INSTALL_HOME}/solr/bin/solr stop
         sleep 30
